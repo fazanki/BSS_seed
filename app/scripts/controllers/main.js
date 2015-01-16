6dd4efec7f8bf2daeebd829a25c679b2d793cf58
Franco Zanki <zankian.vision@gmail.com>
2015-01-16 15:43:53 +0000

    refactoring grunt file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,8 @@
 angular.module('appTestApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'Protractor'
     ];
   });
